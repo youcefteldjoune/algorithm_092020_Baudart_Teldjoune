27cf1d1a7c96c724ab570a0c8e31288f1eb2cad2
youcefteldjoune <39488072+youcefteldjoune@users.noreply.github.com>
2020-10-01 12:24:07 +0200

    Update fibonacci_heap.py

--- fibonacci_heap.py
+++ fibonacci_heap.py
@@ -12,25 +12,81 @@ class Heap(object):
         """
         Ajoute une valeur dans l'arbre
         """
-        pass
+        def insert(self, value: int) -> None:
+        """
+        Ajoute une valeur dans l'arbre
+        """
+        def insert_node(self, value):
+        nouvel_arbre = ArbreFibo(value)
+        self.arbres.append(nouvel_arbre)
+        if (self.least is None or value < self.least.value):
+            self.least = nouvel_arbre
+        self.count = self.count + 1
 
     def find_min(self) -> int:
         """
         Retourne la valeur minimum dans l'arbre
         """
-        pass
+        def find_min(self):
+        if self.least is None:
+            return None
+        return self.least.value
 
     def delete_min(self) -> int:
         """
         Supprime et retourne la valeur minimum dans l'arbre
         """
-        pass
+        def delete_min(self):
+        plus_petit = self.least
+        if plus_petit is not None:
+            for nouveau in plus_petit.nouveau:
+                self.arbres.append(nouveau)
+            self.arbres.remove(plus_petit)
+            if self.arbres == []:
+                self.least = None
+            else:
+                self.least = self.arbres[0]
+                self.consolidate()
+            self.count = self.count - 1
+            return plus_petit.value
+        
+        def consolidate(self):
+        aux = (floor_log(self.count) + 1) * [None]
+        while self.arbres != []:
+            x = self.arbres[0]
+            ancien = x.ancien
+            self.arbres.remove(x)
+            while aux[ancien] is not None:
+                y = aux[ancien]
+                if x.value > y.value:
+                    x, y = y, x
+                x.add_at_end(y)
+                aux[ancien] = None
+                ancien = ancien + 1
+            aux[ancien] = x
+        self.least = None
+        for k in aux:
+            if k is not None:
+                self.arbres.append(k)
+                if (self.least is None
+                        or k.value < self.least.value):
+                    self.least = k
+
 
     def decrease_key(self, current_value: int, new_value :int) -> None:
         """
         Modify une valeur dans l'arbre
         """
-        pass
+        def decrease_key(self, x, k):
+        if k > x.value:
+            return None
+        x.value = k
+        y = x.ancien
+        if y is not None and x.value < y.value:
+            self.cut(x, y)
+            self.cascading_cut(y)
+        if x.value < self.least.value:
+            self.least = x
 
     def merge(self, fibonnaci_heap: object) -> None:
         """
@@ -64,7 +120,7 @@ class FibonacciHeap(Heap):
         """
         Retourne la valeur minimum dans l'arbre
         """
-        def get_min(self):
+        def find_min(self):
         if self.least is None:
             return None
         return self.least.value
@@ -73,7 +129,7 @@ class FibonacciHeap(Heap):
         """
         Supprime et retourne la valeur minimum dans l'arbre
         """
-        def extract_min(self):
+        def delete_min(self):
         plus_petit = self.least
         if plus_petit is not None:
             for nouveau in plus_petit.nouveau:
@@ -86,6 +142,7 @@ class FibonacciHeap(Heap):
                 self.consolidate()
             self.count = self.count - 1
             return plus_petit.value
+        
         def consolidate(self):
         aux = (floor_log(self.count) + 1) * [None]
         while self.arbres != []:
